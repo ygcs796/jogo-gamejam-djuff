618b997061940e12b5940ca45edbb206a92ab63e
ygcs796 <yurigabryel3@gmail.com>
2026-06-06 12:32:49 -0300

    Câmera e cenário
    Coloquei um zoom de 4x na câmera
    Fiz um cenário de 18 x 10 usando tiles de 16 x 16
    Implementei o swap do cenário

---
+++ scripts/fase_1.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+@onready var cenario1 = $cenario1
+@onready var cenario2 = $cenario2
+
+enum State { GROUND, CEILING }
+var state: State = State.GROUND
+
+func _ready():
+	_apply_state()
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"): # swap acontecendo no pulo
+		_swap()
+
+func _swap():
+	if state == State.GROUND:
+		state = State.CEILING
+	else:
+		state = State.GROUND
+	_apply_state()
+
+func _apply_state():
+	# Alterna cenário do mapa
+	cenario1.visible = (state == State.GROUND)
+	cenario2.visible = (state == State.CEILING)
